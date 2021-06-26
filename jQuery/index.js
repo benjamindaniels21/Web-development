5579bcbc67baff69040d4ca38b62c662ffa20da0
Benjamin Daniels <benjamindaniels21@gmail.com>
2021-06-26 17:51:33 -0400

    updates

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,13 +1,35 @@
 
-//add event listener, if h1 is clicked it changes color 
-$("h1").click(function(){
+// //add event listener, if h1 is clicked it changes color
+// $("h1").click(function(){
+//   $("h1").css("color", "purple");
+// });
+
+
+// //button click changes h1 color
+// for(var i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function(){
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+// //use jQuery to do the same color change
+// $("button").click(function(){
+//   $("h1").css("color", "purple");
+// });
+
+
+// //record keypresses in the console
+// $(document).keypress(function(event){
+//   console.log(event.key);
+// });
+
+
+// //change h1 to our keypress
+// $(document).keypress(function(event){
+//   $("h1").text(event.key);
+// });
+
+//
+$(document).on("mouseover",function(){
   $("h1").css("color", "purple");
 });
-
-
-//button click changes h1 color
-for(var i = 0; i < 5; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", function(){
-    document.querySelector("h1").style.color = "purple";
-  });
-}
