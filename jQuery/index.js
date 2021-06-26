bb26ee0072e7b96b08388d3e8ce02262a22914d3
Benjamin Daniels <benjamindaniels21@gmail.com>
2021-06-26 17:34:10 -0400

    update

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,13 @@
-$(document).ready(function{
-  $("h1").css("color", "red");
+
+//add event listener, if h1 is clicked it changes color 
+$("h1").click(function(){
+  $("h1").css("color", "purple");
 });
+
+
+//button click changes h1 color
+for(var i = 0; i < 5; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function(){
+    document.querySelector("h1").style.color = "purple";
+  });
+}
