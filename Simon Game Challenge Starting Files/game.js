182bed52e07b3250f866ad205f6d1463a4c311ea
Benjamin Daniels <benjamindaniels21@gmail.com>
2021-06-26 22:30:36 -0400

    update

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,8 +1,9 @@
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
-function newRand(){
+function nextSequence(){
   var randomNumber = Math.floor((Math.random() * 4));
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
@@ -11,6 +12,22 @@ function newRand(){
 
   var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
   audio.play();
-
-
 };
+
+
+$(".btn").click(function(){
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+});
+
+
+function playSound(name){
+  var randomNumber = Math.floor((Math.random() * 4));
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+}
