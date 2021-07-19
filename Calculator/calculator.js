37ad107e16f635983f68b82a3993c53ee536add9
Benjamin Daniels <benjamindaniels21@gmail.com>
2021-07-19 18:35:42 -0400

    Updated with new BMI calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -8,6 +8,7 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+
 app.post("/", function(req, res){
 
 var num1 = Number(req.body.num1);
@@ -17,6 +18,23 @@ var result = num1 + num2;
 
   res.send("The result is " + result);
 });
+
+
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html")
+});
+
+
+app.post("/bmicalculator", function(req, res){
+  var h = Number(req.body.h);
+  var w = Number(req.body.w);
+
+  var bmi = 703 * w/ (h * h);
+
+  res.send("Your BMI is " + bmi);
+
+});
+
 
 app.listen(3000, function () {
   console.log("server started on port 3000");
